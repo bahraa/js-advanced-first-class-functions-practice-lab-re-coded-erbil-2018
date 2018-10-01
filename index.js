0e4d854bdaee83dfdcfcca531b02647021128a70
bahraa <bbahra98@gmail.com>
2018-10-01 12:52:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,7 @@ const driversByRevenue =function(drivers){
 }
 
 
-const logDriversByHometown = function (drivers, hometown) {
-  drivers.forEach(function (driver) {
-    if (driver.hometown === hometown) {
-      console.log(driver.name);
-    }
-  });
-};
+
 
 
 
