4294bf413857145bf1602bebe4b406013d7d22d1
bahraa <bbahra98@gmail.com>
2018-10-01 12:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,21 @@ const driversByRevenue =function(drivers){
 }
 
 
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
 
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
 
 
 
