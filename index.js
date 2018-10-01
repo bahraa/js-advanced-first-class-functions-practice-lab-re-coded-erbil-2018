0d5a3073c8953daefd3c960488c882f3788e82e5
bahraa <bbahra98@gmail.com>
2018-10-01 12:47:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+
+
+
+function logDriverNames(drivers){
+ drivers.forEach(function (driver,index,array){
+   console.log(driver.name)
+ })
+}
+const logDriversByHometown= function(drivers, location){
+  drivers.forEach(function (el, i, arr){
+    if(el.hometown==location)
+    console.log(el.name);
+})
+}
+
+const driversByRevenue =function(drivers){
+ 
+  return drivers.slice().sort(function(n1,n2){
+    return n1.revenue-n2.revenue;
+  })
+}
+
+
+.n14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
